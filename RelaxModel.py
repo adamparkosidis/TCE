8acc3e49aa42aba425b75541bce6d0abd0eb8eff
hilaglanz <hila@p-m-i.co.il>
2015-12-26 13:28:46 +0200

    M RelaxModel.py

--- RelaxModel.py
+++ RelaxModel.py
@@ -16,9 +16,10 @@ class RelaxedModel:
         for dmParticle in dmParticles:
             evolutionCode.dm_particles.add_particle(dmParticle)
 
-        parts = evolutionCode.gas_particles.copy()
-        centerOfMassRadius = parts.center_of_mass()
-        centerOfMassV = parts.center_of_mass_velocity()
+        gas = evolutionCode.gas_particles.copy()
+        dm = evolutionCode.dm_particles.copy()
+        centerOfMassRadius = gas.center_of_mass()
+        centerOfMassV = gas.center_of_mass_velocity()
 
         print "starting SPH relaxation"
         native_plot.figure(figsize=(10, 10), dpi=60)
@@ -29,25 +30,25 @@ class RelaxedModel:
             print "current time = ", evolutionCode.model_time.as_quantity_in(units.yr)
             print "radius = ", evolutionCode.particles.radius[:-1]
             currentTime += timeStep
-            parts = evolutionCode.gas_particles.copy()
-            sph_particles_plot(parts)
+            gas = evolutionCode.gas_particles.copy()
+            dm = evolutionCode.dm_particles.copy()
+            sph_particles_plot(gas)
             # native_plot.show()
             #native_plot._imsave(string.format("relax_{0}", currentTime))
 
             f = ((currentTime/timeStep) * 1.0)/timeSteps
-            parts.add_particle(evolutionCode.dm_particles)
-            evolutionCode.gas_particles.position += centerOfMassRadius - parts.center_of_mass()
-            evolutionCode.dm_particles.position += centerOfMassRadius - parts.center_of_mass()
-            evolutionCode.gas_particles.velocity = f * (evolutionCode.gas_particles.velocity - parts.center_of_mass_velocity()) + centerOfMassV
-            evolutionCode.dm_particles.velocity = f * (evolutionCode.dm_particles.velocity - parts.center_of_mass_velocity()) + centerOfMassV
+            gas.add_particle(evolutionCode.dm_particles)
+            evolutionCode.gas_particles.position += centerOfMassRadius - gas.center_of_mass()
+            evolutionCode.dm_particles.position += centerOfMassRadius - gas.center_of_mass()
+            evolutionCode.gas_particles.velocity = f * (evolutionCode.gas_particles.velocity - gas.center_of_mass_velocity())\
+                                                   + centerOfMassV
+            evolutionCode.dm_particles.velocity = f * (evolutionCode.dm_particles.velocity - gas.center_of_mass_velocity())\
+                                                  + centerOfMassV
 
+            gas = evolutionCode.gas_particles.copy()
+            dm = evolutionCode.dm_particles.copy()
         # TODO: should I change the center of mass?
 
         evolutionCode.stop()
-        self.gas_particles = evolutionCode.gas_particles.copy()
-        self.dm_particles = evolutionCode.dm_particles.copy()
-        native_plot.figure(figsize=(10, 10), dpi=60)
-        sph_particles_plot(parts)
-        native_plot.show()
-        sph_particles_plot(self.gas_particles)
-        native_plot.show()
+        self.gas_particles = gas
+        self.dm_particles = dm

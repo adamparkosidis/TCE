19957f4aa914898be5ff3f3885a24a819de4a520
Hila Laptop <hila@p-m-i.co.il>
2020-06-17 18:43:04 +0300

    script to concate data files of sequential snapshots

---
+++ ConcateFiles.py
@@ -0,0 +1,64 @@
+import os, sys, glob
+import argparse
+
+def InitParser():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--first', type=str, help='first day')
+    parser.add_argument('--last', type=str, help='last day')
+    parser.add_argument('--name', type=str, help='name of files without times')
+    parser.add_argument('--source_dir', type=str, help='path to files directory')
+    return parser
+
+def GetLastDayOfFile(fileName):
+    return fileName.split('_to_')[-1].split('days')[0]
+
+def GetListOfFilesToConcat(timesDict, curr_first, last):
+    if curr_first == last:
+        return []
+
+    if curr_first not in timesDict:
+        print curr_first, " is not the first in any file"
+        return None
+
+    for f in timesDict[curr_first]:
+        l = GetListOfFilesToConcat(timesDict, str(float(GetLastDayOfFile(f)) + 0.2), last)
+        if l is not None:
+            return [f] + l
+
+        l = GetListOfFilesToConcat(timesDict, str(float(GetLastDayOfFile(f)) + 0.1), last)
+        if l is not None:
+            return [f] + l
+
+    return None
+
+def ConcateFiles(files):
+    data = ""
+    first = True
+    for f in files:
+        file = open(f,mode="r")
+        if not first:
+            data += ", "
+        data += file.readline()
+        first = False
+
+    return data
+
+if __name__ == "__main__":
+    parser = InitParser()
+    args = parser.parse_args()
+    files = glob.glob(args.source_dir + "\\" + args.name + "*")
+    begintimeDict = dict()
+    for f in files:
+        beginTime = f.split('time_')[-1].split('_to')[0]
+        if beginTime not in begintimeDict:
+            begintimeDict[beginTime] = []
+        begintimeDict[beginTime].append(f)
+
+    filesToConcat = GetListOfFilesToConcat(begintimeDict, args.first, args.last)
+    if filesToConcat is None:
+        print "could not concat those files..."
+    else:
+        newFile = open(args.source_dir + "\\" + args.name + "time_" + args.first + "_to_" + args.last, mode="w")
+        newFile.write(ConcateFiles(filesToConcat))
+        newFile.close()
+        print "new file has been saved to ", args.source_dir + "\\" + args.name + "time_" + args.first + "_to_" + args.last
